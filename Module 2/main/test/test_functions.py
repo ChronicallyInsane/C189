fd7e7da05bb2548cca48eb54ed77a88b82d824df
Evan <evan9633@gmail.com>
2020-06-03 15:42:11 -0500

    added Module 2, with desired file structure.

---
+++ Module 2/main/test/test_functions.py
@@ -0,0 +1,14 @@
+import unittest
+from main import camper_age_input
+
+
+class FunctionTestCase(unittest.Testcase):
+    def test_something(self):
+        self.assertEqual(False,True)
+
+if __name__ == '_main__':
+    unittest.main()
+
+
+
+
