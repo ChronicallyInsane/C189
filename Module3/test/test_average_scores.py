453b7cfa0ba57e4c7605e64fd7d7c0f820094a79
Evan <evan9633@gmail.com>
2020-06-08 09:41:53 -0500

    Add files via upload

--- Module3/test/test_average_scores.py
+++ Module3/test/test_average_scores.py
@@ -9,10 +9,15 @@ import unittest
 import unittest.mock as mock
 
 
-def test_average(self):
-    with mock.patch('builtins.input', side_effects=[85, 90, 95]):
-        assert average_scores.average() == 90
-    with mock.patch('buildins.input', side_effects=[42, 25, 56]):
-        assert average_scores.average() == 41
-    with mock.patch('buildins.input', side_effects=[95, 25, 56]):
-        assert average_scores.average() == 41
+class MyTestCase(unittest.TestCase):
+    def test_average(self):
+        # with mock.patch('builtins.input', side_effects=[85, 90, 95]):
+        assert average_scores.average(85, 90, 95) == 90
+        # with mock.patch('buildins.input', side_effects=[42, 25, 56]):
+        assert average_scores.average(42, 25, 56) == 41
+        # with mock.patch('buildins.input', side_effects=[95, 25, 56]):
+        assert average_scores.average(95, 25, 56) == 41
+
+
+if __name__ == '__main__':
+    unittest.main()
