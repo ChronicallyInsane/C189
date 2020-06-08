84780cf5cd781bd6c124a04e0b82d300da6272ed
Evan <evan9633@gmail.com>
2020-06-08 09:31:23 -0500

    Add files via upload

---
+++ Module3/test/test_average_scores.py
@@ -0,0 +1,17 @@
+"""
+Author:evan
+Purpose: To test the average function
+date:6/7
+"""
+
+from Module3.format_output import average_scores
+import unittest
+import unittest.mock as mock
+
+
+def test_average(self):
+    with mock.patch('builtins.input', side_effects=[85, 90, 95]):
+        assert average_scores.average() == 90
+    with mock.patch('buildins.input', side_effects=[42, 25, 56]):
+        assert average_scores.average() == 41
+
