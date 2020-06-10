589541ed2dcd26deb6e5c215d02596516ffdd7c1
Evan <evan9633@gmail.com>
2020-06-10 09:20:45 -0500

    updated tests
    ...

--- Module4/input_validation/validation_with_try.py
+++ Module4/input_validation/validation_with_try.py
@@ -1,10 +1,18 @@
 import Module3.format_output.average_scores
 
+
 def main():
-    pass
+    average(-1, 3, 4)
 
 
-
+def average(one, two, three):
+    if one < 0 or two < 0 or three < 0:
+        raise ValueError
+        exit()
+    else:
+        total = one + two + three
+    NUM_TESTS = 3
+    return (total / NUM_TESTS)
 
 
 if __name__ == '__main))':
--- Module4/test/test_validation_with_try.py
+++ Module4/test/test_validation_with_try.py
@@ -1,14 +1,15 @@
 import unittest
 
-import Module3.format_output.average_scores
+from Module3.format_output import average_scores
+from Module4.input_validation.validation_with_try import average
 from Module3.test.test_average_scores import average_scores
 
 
 class MyTestCase(unittest.TestCase):
     def test_average_exception(self):
         with self.assertRaises(ValueError):
-            average_scores.average(-90, 89, 78)
+            average(-90, 89, 78)
 
     def test_average_negative_input(self):
-        with self.ssertRaises(ValueError):
-            average_scores.average(-95, 89, 78)
+        with self.assertRaises(ValueError):
+            average(-95, 89, 78)
