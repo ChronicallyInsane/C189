7730fe542fde4685c0ba2b58454125fbad3c244b
Evan <evan9633@gmail.com>
2020-06-18 12:44:36 -0500

    removed assertRaises because I suck at using it

--- Module6/test_functions/test_validate_input_in_functions.py
+++ Module6/test_functions/test_validate_input_in_functions.py
@@ -4,7 +4,7 @@ from Module6.more_functions import validate_input_in_functions as val
 
 class MyTestCase(ut.TestCase):
     def test_score_input_test_name(self):
-        self.assertEqual(val.score_input("test_name"), "test_name: 0")  # tests name, and mandatory functions
+        self.assertEquals(val.score_input("test"), "test: 0")  # tests name, and mandatory functions
 
     def test_score_input_test_score_valid(self):
         self.assertEqual(val.score_input("name", 50), "name: 50")
@@ -16,7 +16,10 @@ class MyTestCase(ut.TestCase):
         self.assertEqual(val.score_input("name", 101), -1)
 
     def test_test_score_non_numeric(self):
-        self.assertRaises(val.score_input("test", "test"))
+        self.assertEqual(val.score_input("test", "test"), -1)
+
+    def test_score_input_invalid_message(self):
+        self.assertEqual(val.score_input("name", 50, 50), -1)
 
 
 if __name__ == '__main__':
