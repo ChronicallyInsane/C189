59e1b7ea34bada9f2017fb85add5c739a11a7dfe
Evan <evan9633@gmail.com>
2020-06-18 09:17:04 -0500

    Add files via upload

---
+++ Module6/more_functions/string_functions.py
@@ -0,0 +1,17 @@
+"""
+Author: Evan
+Date: 6/17
+Purpose: expansion on string handling
+"""
+
+
+def main():
+    pass
+
+
+def multiply_string():
+    pass
+
+
+if __name__ == '__main__':
+    main()
