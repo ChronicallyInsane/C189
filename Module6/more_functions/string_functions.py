b5ccc4e5f62636790a120a825a61365caf8c0fa7
Evan <evan9633@gmail.com>
2020-06-18 10:08:39 -0500

    finalized String_functions and tests

--- Module6/more_functions/string_functions.py
+++ Module6/more_functions/string_functions.py
@@ -6,11 +6,15 @@ Purpose: expansion on string handling
 
 
 def main():
-    pass
+    print(multiply_string("apple", 5))
 
 
-def multiply_string():
-    pass
+def multiply_string(string, mult_value):
+    if str.isascii(string) != 1:
+        print("enter correct string")
+        exit(-1)
+
+    return string * int(mult_value)
 
 
 if __name__ == '__main__':
