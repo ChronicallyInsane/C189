050ed54d7922a171de6b4aa501b90bfd0f47d407
ChronicallyInsane <bobjote123>
2020-06-25 11:39:39 -0500

    still buggy

--- Module8/more_fun_with_collections/dict_membership.py
+++ Module8/more_fun_with_collections/dict_membership.py
@@ -3,6 +3,7 @@ Author: Evan
 Date: 6/23
 Purpose; advanced implementation of dictionaries
 """
+from string import ascii_lowercase
 
 
 def main():
@@ -12,9 +13,9 @@ def main():
     print("Average Score: ", average_scores(get_test_scores()))
 
 
-def in_dict(dict_t, value):
-    for i in dict_t:
-        if i == value:
+def in_dict(dict_t, val):
+    for key, value in dict_t:
+        if val == dict_t[key]:
             return True
     return False
 
@@ -24,26 +25,32 @@ def get_test_scores():
     num_scores = int(input("input desired number of scores"))
     i = 0
     while i <= num_scores:
-        ind = input("input score")
-        if ind < 0 or ind > 100:
-            print("invalid score, try again.")
-            continue
-        try:
-            str.capitalize(ind)
-        except ValueError:
-            print("invalid entry, try again.")
-            continue
-        scores_dict.update(i, ind)
         i += 1
+        ind = input("input score")
+        try:
+            int(ind) / 2
+        except TypeError:
+            print("invalid entry, try again")
+            i -= 1
+            continue
+        finally:
+
+            if int(ind) < 0 or int(ind) > 100:
+                print("invalid score, try again.")
+                i -= 1
+                continue
+            scores_dict.update({ascii_lowercase[i]: "%s".format(ind)})
+            continue
+
     return scores_dict
 
 
 def average_scores(s_dict):
     total = 0
-    for value in s_dict.items():
-        total += value
+    for key, value in s_dict.items():
+        total += int(value)
     return total / len(s_dict)
 
 
-if __name__ == '__name__':
+if __name__ == '__main__':
     main()
