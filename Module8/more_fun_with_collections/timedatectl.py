3f32e60657929a03ebe0d59e1cb4c8849e21b628
ChronicallyInsane <bobjote123>
2020-06-27 11:02:02 -0500

    date

---
+++ Module8/more_fun_with_collections/timedatectl.py
@@ -0,0 +1,21 @@
+from datetime import datetime, timedelta
+
+def main():
+    # Current time
+    time_now = datetime.now()
+
+    print("Time now", str(time_now))
+
+    # Calculating for time travel... 1 year in the future
+    one_year_later = time_now + timedelta(days=365)
+    two_days_later = time_now + timedelta(days=2)
+
+    # Print future dates
+    print('one_year_later:', str(one_year_later))
+    print('two_days_later:', str(two_days_later))
+
+
+def half_birthday(date):
+    bday = date + timedelta(days=(365/2))
+    return bday
+
---
+++ Module8/test/test_half_birthday.py
@@ -0,0 +1,9 @@
+from Module8.more_fun_with_collections import dict_membership
+import unittest as ut
+
+
+class TestList(ut.TestCase):
+
+
+if __name__ == '__main__':
+    ut.main()
