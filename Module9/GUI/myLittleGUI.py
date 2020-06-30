47ad3581791ebe9be58400c75ef17a866180a00e
ChronicallyInsane <bobjote123>
2020-06-30 13:21:09 -0500

    BUTTONS

--- Module9/GUI/myLittleGUI.py
+++ Module9/GUI/myLittleGUI.py
@@ -13,18 +13,18 @@ def main():
     label = tkinter.Label(context, text="No Selection Made")
 
     def on_button_click():
-        label.config(text="Eat More Protein!")
+        label.config(text="Eat More Protein!") #doesn't work properly
 
-    var1 = tkinter.IntVar()
-    check = tkinter.Checkbutton(context, text="Breakfast", variable=var1).grid(row=0)
+    var1 = tkinter.IntVar() # options
+    check = tkinter.Checkbutton(context, text="Breakfast", variable=var1, command=on_button_click()).grid(row=0)
     var2 = tkinter.IntVar()
-    check = tkinter.Checkbutton(context, text="2nd Breakfast", variable=var2).grid(row=1)
+    check = tkinter.Checkbutton(context, text="2nd Breakfast", variable=var2, command=on_button_click()).grid(row=1)
     var3 = tkinter.IntVar()
     check = tkinter.Checkbutton(context, text="Lunch", variable=var3).grid(row=2)
     var4 = tkinter.IntVar()
     check = tkinter.Checkbutton(context, text="Dinner", variable=var4).grid(row=3)
     button = tkinter.Button(context, text='ANNIHILATE', width=30, command=context.destroy).grid(row=6)
-    context.mainloop()
+    context.mainloop()#initialize main loop for decision
 
 
 if __name__ == '__main__':
