e8b32157ea5ff8c087e59c8235fab947ed4798a5
ChronicallyInsane <bobjote123>
2020-07-24 10:56:12 -0500

    finished Student/Person classes

--- Module11/Person.py
+++ Module11/Person.py
@@ -3,9 +3,12 @@ Evan
 Date: Too late
 Purpose: student/person interaction.  Person design copied as instructed.
 """
+from datetime import datetime, timedelta
+
 
 class Person:
     """Person class using class Address, Class Composition"""
+
     def __init__(self, lname, fname, addy=''):
         name_characters = set("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'-")
         if not (name_characters.issuperset(lname) and name_characters.issuperset(fname)):
@@ -15,11 +18,13 @@ class Person:
         self.address = addy
 
     def display(self):
-        return str(self.last_name) + ", " + str(self.first_name) + '\n'
-               + self.address.display()
+        return str(self.last_name) + ", " + str(self.first_name) + '\n' + self.address
+
+
 class Student:
     """Student string extending person"""
-    def __init__(self, person, major="no major", gpa=0.00, start_date):
+
+    def __init__(self, person, major="no major", gpa=0.00, start_date="00/00/2000"):
         self.Person = person
         self.major = major
         self.gpa = gpa
@@ -31,5 +36,19 @@ class Student:
     def update_gpa(self, n_gpa):
         self.gpa = n_gpa
 
+    def update_date(self):
+        self.date = datetime.now()
+
     def display(self):
-        print(self.Person.display(), "\nGPA: {}".format(self.gpa)+ "\n Major:{}".format(self.major))
+        print(self.Person.display(),
+              "\nGPA: {}".format(self.gpa) + "\n Major:{}".format(self.major) + "\n Start Date:{}".format(self.date))
+
+
+the_dude = Person("dude", "the", "like, nowhere man")
+
+poor_student = Student(the_dude, "vibing, bro", 4.00)
+poor_student.change_major("Being Awesome!")
+poor_student.update_gpa(3.00)
+poor_student.display()
+del poor_student
+del the_dude
